cae376fcc2525abd3107650f6f110b71a65cf453
KylinDC <inlovingdays@gmail.com>
2019-01-09 00:37:30 +0800

    add the feature of show different todolist

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ let viewStateArr = ["all", "active", "completed"];
 let viewState = viewStateArr[0];
 
 window.onload = () => {
-  showTodoList();
+  showAllTodoList();
   showItemsInfo();
 };
 
-function showTodoList(viewState = viewStateArr[0]) {
+function showTodoList() {
   let todoListHTML = "";
   switch (viewState) {
     case "all":
@@ -39,5 +39,49 @@ function showItemsInfo() {
     clearButton.classList.remove("hidden");
   } else {
     clearButton.classList.add("hidden");
+  }
+}
+
+function showAllTodoList() {
+  viewState = viewStateArr[0];
+  changeButtonState();
+  showTodoList();
+}
+
+function showActiveTodoList() {
+  viewState = viewStateArr[1];
+  changeButtonState();
+  showTodoList();
+}
+
+function showCompletedTodoList() {
+  viewState = viewStateArr[2];
+  changeButtonState();
+  showTodoList();
+}
+
+function changeButtonState() {
+  let allButton = document.getElementById("all_button");
+  let activeButton = document.getElementById("active_button");
+  let completedButton = document.getElementById("completed_button");
+
+  switch (viewState) {
+    case "all":
+      allButton.classList.add("selected_button");
+      activeButton.classList.remove("selected_button");
+      completedButton.classList.remove("selected_button");
+      break;
+    case "active":
+      allButton.classList.remove("selected_button");
+      activeButton.classList.add("selected_button");
+      completedButton.classList.remove("selected_button");
+      break;
+    case "completed":
+      allButton.classList.remove("selected_button");
+      activeButton.classList.remove("selected_button");
+      completedButton.classList.add("selected_button");
+      break;
+    default:
+      break;
   }
 }
